0643866fd4f255f2961884d55b74c7cb9efc6b08
vijay <vijaybabu4589@gmail.com>
2018-11-15 22:31:17 +0530

    Update test_proj.ino

--- test_proj/test_proj.ino
+++ test_proj/test_proj.ino
@@ -105,7 +105,7 @@ void senddata(){
   
               HTTPClient http;
   
-              http.begin("http://192.168.0.100/?dataa=vijaya");
+              http.begin("http://192.168.0.100/add_volume?value=vijaya");
               //http.addHeader("Content-Type", "application/json\r\n");
               
               int httpCode = http.GET();
